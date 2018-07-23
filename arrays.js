6bd271afec81004347a1b2aa165d35181e9f88f2
Noelle Rolwes <nrolwes@gmail.com>
2018-07-23 14:02:55 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,5 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+var newChocolateBars = []
 
 function addElementToBeginningOfArray(array, element){
  return [element,...array]
@@ -16,6 +17,9 @@ function destructivelyAddElementToEndOfArray(array, element){
   return [array.push(element)]
  
 }
+
+function accessElementInArray(array, index)
+  return array[]
 
 console.log(addElementToBeginningOfArray(chocolateBars, "Payday"))
 console.log(destructivelyAddElementToBeginningOfArray(chocolateBars, "Payday"))
