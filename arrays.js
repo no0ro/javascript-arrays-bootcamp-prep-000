5b5108dd61fc70115aa96ae84c9073ec34463cd2
Noelle Rolwes <nrolwes@gmail.com>
2018-07-23 14:21:37 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,7 +10,7 @@ function destructivelyAddElementToBeginningOfArray(array, element){
 }
 
 function addElementToEndOfArray(array, element){
-  return array[1]
+  return array[1
 }
 
 function destructivelyAddElementToEndOfArray(array, element){
