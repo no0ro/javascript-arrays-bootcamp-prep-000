028ae55202572951f860e572957caf6212a7decc
Noelle Rolwes <nrolwes@gmail.com>
2018-07-23 13:42:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,16 +5,16 @@ function addElementToBeginningOfArray(array, element){
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  return array.unshift(element)
+  return [array.unshift(element)]
 }
 
 function addElementToEndOfArray(array, element){
-  return[array,...element]
+  return [array,...element]
 }
 
 function destructivelyAddElementToEndOfArray(array, element){
-  array.push(element)
-  return array;
+  return [array.push(element)]
+ 
 }
 
 console.log(addElementToBeginningOfArray(chocolateBars, "Payday"))
